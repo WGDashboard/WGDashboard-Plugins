8b14caed928309c96eb134ecee2378daf0014db0
Donald Zou <donaldzou@live.hk>
2025-08-14 10:39:18 +0800

    Update plugin template

--- plugin_template/main.py
+++ plugin_template/main.py
@@ -1,2 +1,5 @@
+from modules import module1
+
 def main(WireguardConfigurations: dict[str, object]):
     print("Hi! This is a plugin template!")
+    module1.module1()
---
+++ plugin_template/modules/module1.py
@@ -0,0 +1,2 @@
+def module1():
+    print("Hi! I am a module and can be imported")
