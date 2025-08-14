5f5733601e0a4320017a9dda9b45c5db432ec068
Donald Zou <donaldzou@live.hk>
2025-08-14 16:51:10 +0800

    Remove testing files

--- plugin_template/assets/stores/store.ts
+++
@@ -1,8 +0,0 @@
-import { defineStore } from 'pinia'
-
-export const store = defineStore('store', () => {
-    const modal = ref(false)
-    const modalSrc = ref("")
-
-    return {modal, modalSrc}
-})
